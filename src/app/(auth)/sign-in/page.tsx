168d2f95ac3f49dd751c8b11584ed777b6fd3bcf
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-12-25 19:10:09 +0500

    sign in page Input

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -5,3 +5,6 @@ import {
   Button,
   buttonVariants,
 } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { cn } from '@/lib/utils'
