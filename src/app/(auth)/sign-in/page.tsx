34e6cde8be46e052f412c9281155e0f2e0ba19fc
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-12-26 20:49:07 +0500

    adding zodResolver

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -8,3 +8,8 @@ import {
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { cn } from '@/lib/utils'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ArrowRight, Loader2 } from 'lucide-react'
+import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+
